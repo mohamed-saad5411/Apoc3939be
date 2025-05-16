80847fca3386168be422d1998394f2de9d893f16
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-05-17 02:15:22 +0300

    deploy phase one

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
 
-export const dbConnection = () => { mongoose.connect(process.env.LOCAL_HOST) }
-// export const dbConnection = () => { mongoose.connect(process.env.LIVE_HOST) }
+// export const dbConnection = () => { mongoose.connect(process.env.LOCAL_HOST) }
+export const dbConnection = () => {"mongodb+srv://Apoc3939:aboc3939@cluster0.z6yv1cg.mongodb.net/Apoc3939" }
 
 
