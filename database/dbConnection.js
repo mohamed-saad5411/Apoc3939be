1a64cf614b86fbfb75470a7a61924ef69cd3e8a3
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-05-18 00:37:50 +0300

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 
+export const dbConnection = async () => {
+    try {
+        await mongoose.connect(mongodb+srv://Apoc3939:aboc3939@cluster0.z6yv1cg.mongodb.net/Apoc3939);
+    } catch (error) {
+        console.log(error);
+    }
 
+}
 
 
 // export const dbConnection = () => { mongoose.connect(process.env.LOCAL_HOST) }
-export const dbConnection = () => {"mongodb+srv://Apoc3939:aboc3939@cluster0.z6yv1cg.mongodb.net/Apoc3939" }
+// export const dbConnection = () => {"mongodb+srv://Apoc3939:aboc3939@cluster0.z6yv1cg.mongodb.net/Apoc3939" }
 
 
