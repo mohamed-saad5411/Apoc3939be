b995852d5751f38d68685c61cd8eab9026fa47b0
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-05-18 01:24:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-import express from 'express'
+import express, { json } from 'express'
+import mongoose from "mongoose";
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { dbConnection } from './database/dbConnection.js'
-import userRouter from './src/modules/user/user.router.js'
+import { userModel } from './database/model/user.model.js';
 const app = express()
 const port = 3000
 dotenv.config()
+
 app.use(cors())
 app.use(express.json())
-app.use('/api/v1/user' , userRouter)
+
 app.get('/', (req, res) => res.send('Hello connected api!'))
+app.get('/users', async (req, res) => {
+    let users = await userModel.find()
+    res.json({ message: 'success', users })
+})
+app.post('/register', (async (req, res) => {
+    const { name, email, payment, phone } = req.body;
+    const user = await userModel.insertMany({ name, email, payment, phone });
+    res.json({ Message: 'success', user });
+    // let isExist = await userModel.findOne({email})
+    // if (!isExist) {
+    // }else{
+    //     res.json({ Message: 'user is already exist' });
+    // }
+}))
 
 
+const dbConnection = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://Apoc3939:aboc3939@cluster0.qwetbng.mongodb.net/Apoc3939");
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 dbConnection()
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
