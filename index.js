e812a2ca7591c974365ad3f6819fdf5dad0dd8c4
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-05-20 05:33:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express'
-const app = express()
-const port = 3000
+import { dbConnection } from './database/dbconnection.js'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import userRouter from './src/modules/user/user.router.js'
 
+const app = express()
+const port = 5000
+
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/api/v1', userRouter)
+
+
+dbConnection()
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
